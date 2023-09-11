e18f4abbcd7259015c0c820080920398edd5ff25
biah.paixão <108343635+biahpaixao16@users.noreply.github.com>
2023-09-11 08:01:16 -0300

    .

--- main.js/main.js
+++ main.js/main.js
@@ -1,7 +1,7 @@
 
-function tocasompom () {
+function TocaSomPom () {
     document.querySelector('som_tecla_pom').play();
 }
 
 
-    document.querySelector('.tecla_pom')
+    document.querySelector('.tecla_pom').onclick = TocaSomPom
