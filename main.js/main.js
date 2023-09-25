61c489dc03bcd1b8c36c82ba1e1c209af937e96f
Giovane <103274655+Giovane166@users.noreply.github.com>
2023-09-25 08:19:34 -0300

    main.js

--- main.js/main.js
+++ main.js/main.js
@@ -1,17 +1,39 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-function Tocar () {
-    document.querySelector('som_tecla_pom').play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
-    const ListadeTeclas = document.querySelectorAll(' .tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-   let Contador 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-let contador 0;                                                                                                                                
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    while (contador < ListadeTeclas.length) {
-        ListadeTeclas[Contador].onclick = function () {
-            tocar('#som_tecla_pom');
-        contador = contador + 1;
-        console.log(contador);
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
